30505bf4c20961fbc1c30bbb17019df987489630
CHIKURA Shinsaku <shinsaku@chikura.me>
2014-05-27 23:53:40 +0900

    Swap module definition and require lines.

--- lib/todoist.rb
+++ lib/todoist.rb
@@ -2,6 +2,16 @@ require 'uri'
 require 'json'
 require 'faraday'
 
+module Todoist
+  class InvalidURIError < StandardError; end
+  class InvalidJSONPathError < StandardError; end
+  class InvalidJSONError < StandardError; end
+  def configure
+    raise InvalidArgument.new unless block_given?
+    yield(Configuration)
+  end
+end
+
 require 'todoist/http'
 require 'todoist/configuration'
 
@@ -12,13 +22,3 @@ require 'todoist/note'
 require 'todoist/project_items'
 
 require 'todoist/version'
-
-module Todoist
-  class InvalidURIError < StandardError; end
-  class InvalidJSONPathError < StandardError; end
-  class InvalidJSONError < StandardError; end
-  def configure
-    raise InvalidArgument.new unless block_given?
-    yield(Configuration)
-  end
-end
